99c2adc9ed48e4c666d409607b72dc98bdded726
june <ajsjdlwj0123@gmail.com>
2023-03-21 23:19:11 +0900

    [PGS] 교점에 별 만들기

---
+++ ParkJuneYub/[PGS]교점에_별_만들기.java
@@ -0,0 +1,42 @@
+import my.Myutil.Solution.Point;
+
+class Solution {
+	
+	private static class Point { // 좌표 클래스
+		
+		public final long x, y; 
+		
+		private Point(long x, long y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+	private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2) {
+		// 교점 구해서 반환하기
+		return null;
+	}
+	
+    public String[] solution(int[][] line) {
+    	
+    	
+    	
+    	// 모든 직선 쌍에 대해 반복
+    	for(int i = 0; i < line.length(); i++) {
+    		for(int j = 0; j < line.length(); j++) {
+    			
+    		}
+    	}
+    	// 교점좌표 구하고 정수 좌표만 저장
+    	
+    	// 저장된 정수들에 대해 X, Y 좌표의 최댓값, 최솟값 구하기
+    	
+    	// 구한 최댓값 최솟값을 이용하여 2차원 배열의 크기 결정
+    	
+    	// 별표시
+
+    	
+        String[] answer = {};
+        return answer;
+    }
+}
